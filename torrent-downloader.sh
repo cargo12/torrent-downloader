bdee3224f6aa58a2411f5d747e6aa3da02e4f9f3
Mahmood <mahmoodrm@gmail.com>
2013-02-12 11:56:56 +0330

    Update torrent-downloader.sh

--- torrent-downloader.sh
+++ torrent-downloader.sh
@@ -3,6 +3,20 @@
 # Automatically downloads torrent file from a torrent portal
 # for use by Transmission
 
+#    This program is free software; you can redistribute it and/or
+#    modify it under the terms of the GNU General Public License as
+#    published by the Free Software Foundation; either version 2 of
+#    the License or (at your option) version 3 or any later version.
+#
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program; if not, see http://www.gnu.org/licenses/
+
 # Set variables
 today=$(date '+%Y-%m-%d')
 #today="2012-03-06"
